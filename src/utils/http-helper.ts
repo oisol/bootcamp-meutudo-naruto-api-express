8fc67dfc96b4d12fae562b6a5808f3f8ee1e31dc
Matheus <matheusrocha.dev01@gmail.com>
2025-08-24 02:19:36 -0300

    refactor: correct HttpResponse type

--- src/utils/http-helper.ts
+++ src/utils/http-helper.ts
@@ -1,16 +1,23 @@
-interface HttpResponde {
+interface HttpResponse {
   statusCode: number;
   body: any;
 };
 
-export const ok = async (data: any): Promise<HttpResponde> => {
+export const ok = async (data: any): Promise<HttpResponse> => {
   return {
     statusCode: 200,
     body: data
   }
 }
 
-export const noContent = async (data: any): Promise<HttpResponde> => {
+export const created = async (data: any): Promise<HttpResponse> => {
+  return {
+    statusCode: 201,
+    body: data
+  }
+}
+
+export const noContent = async (data: any): Promise<HttpResponse> => {
   return {
     statusCode: 204,
     body: null
